470b2d728ec7b054d15f08e3bb4c1677a3ffe5f7
Sahrul <sahrul.mustakim@indochat.co.id>
2022-04-12 14:27:33 +0700

    updates

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['loggedin'])) {
 </html>
 <script>
 document.addEventListener("message", function(data) {
-	alert(JSON.stringify(data))
+	alert(JSON.stringify(data.data))
     var username = data.data.username
 	var password = data.data.password
 	// alert(username+' - '+password)
