19df240d3345c1372aafe5f551c9f819f6556d18
Sahrul <sahrul.mustakim@indochat.co.id>
2022-04-13 10:29:46 +0700

    updates

--- home.php
+++ home.php
@@ -19,22 +19,20 @@ if (!isset($_SESSION['loggedin'])) {
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Website Title</h1>
+				<h1>Homepage</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Home Page</h2>
 			<p>Welcome back, <?=$_SESSION['name']?>!</p>
-
 			<div id="login">
 				<form id="formLogin" action="authenticate.php" method="post">
 					<div class="form-control">
 						<h3 style="margin-top: 0px;margin-bottom: 5px;">Message</h3>
 					</div>
 					<div class="form-control">
-						<textarea style="margin-bottom: 15px;width: 100%;" name="message" id="message" cols="30" rows="5"></textarea>
+						<textarea style="margin-bottom: 15px;width: 100%;height: 100px;" name="message" id="message"></textarea>
 					</div>
 					<input type="button" value="Send Message">
 				</form>
