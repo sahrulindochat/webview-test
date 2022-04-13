f4d9a8011afa495b9cc273a37ff8cb3f44ca2c64
Sahrul <sahrul.mustakim@indochat.co.id>
2022-04-13 10:56:32 +0700

    updates

--- home.php
+++ home.php
@@ -41,7 +41,7 @@ if (!isset($_SESSION['loggedin'])) {
 <script>
 document.addEventListener("message", function(result) {
 	let resultData = JSON.parse(result.data)
-	let resultData = JSON.parse(result.data)
+	alert(resultData)
 	if(resultData.type == 'message'){
 		document.getElementById("message").innerHTML = resultData.message;
 	}
